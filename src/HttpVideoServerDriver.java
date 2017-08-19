05f6686c0d431d858f6de0294f8e9425e0fdeece
ccoseyxula <ccosey@xula.edu>
2017-08-19 14:19:46 -0500

    Added Comments to HttpVideoServerDriver.java

--- src/HttpVideoServerDriver.java
+++ src/HttpVideoServerDriver.java
@@ -1,5 +1,13 @@
 public class HttpVideoServerDriver {
+
     public static void main(String[] args) {
 
+        //Get server files paths
+
+        //Setup server
+
+        //start server
+
     }
+
 }
