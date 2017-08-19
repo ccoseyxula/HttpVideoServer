7b096efdd6e57d49bf28080070234323a946b4d6
ccoseyxula <ccosey@xula.edu>
2017-08-19 14:28:44 -0500

    Added global final var rootPath:String to HttpVideoServerDriver.java

--- src/HttpVideoServerDriver.java
+++ src/HttpVideoServerDriver.java
@@ -1,5 +1,7 @@
 public class HttpVideoServerDriver {
 
+    private final String rootPath = "/serverFiles/rootPage.html";
+
     public static void main(String[] args) {
 
         //Get server files paths
